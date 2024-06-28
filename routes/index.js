2a2da074177a6833a006fce4bcd68cb8553038c6
TranAnhVu0411 <vuta183674@gmail.com>
2024-06-28 09:43:26 +0700

    renew repository

--- routes/index.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router(),
-userRoutes = require('./userRoutes'),
-salerRoutes = require('./salerRoutes');
-
-router.use('/salers', salerRoutes);
-router.use('/users', userRoutes);
-
-module.exports = router;
--- routes/userRoutes.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router(),
-usersController = require('../controllers/usersController');
-
-router.get('/', usersController.index, usersController.indexView);
-router.get('/new', usersController.new);
-router.post('/create', usersController.validate, usersController.create, usersController.redirectView);
-
-module.exports = router;
