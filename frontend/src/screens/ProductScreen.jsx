1b99155c8d737102472cc4bdfe4f88b2f5c37462
TranAnhVu0411 <vuta183674@gmail.com>
2024-06-28 21:36:45 +0700

    Update Frontend code
    Get Products and Product info from Backend instead of Frontend

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -1,12 +1,20 @@
+import { useEffect, useState } from 'react';
 import { useParams, Link } from "react-router-dom"
 import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap'
+import axios from 'axios';
 import Rating from '../components/Rating'
-import products from "../products"
 
 const ProductScreen = () => {
+    const [product, setProduct] = useState({});
     const { id: productId } = useParams();
-    const product = products.find((p) => p._id === productId);
-    console.log(product);
+
+    useEffect(() => { 
+        const fetchProduct = async () => {
+            const { data } = await axios.get(`/api/products/${productId}`);
+            setProduct(data);
+        }
+        fetchProduct();
+    }, [productId]);
 
     return (
         <>
