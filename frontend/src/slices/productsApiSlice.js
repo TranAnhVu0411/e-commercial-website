972df58a9003d0808d46e015991b649f3b4b6d85
TranAnhVu0411 <vuta183674@gmail.com>
2024-06-29 22:15:54 +0700

    Add State Management for Product in FrontEnd
    Remove UseState, UseEffect and axios, instead, use State Management from redux and redux toolkit

---
+++ frontend/src/slices/productsApiSlice.js
@@ -0,0 +1,21 @@
+import { PRODUCT_URL } from '../constants';
+import { apiSlice } from './apiSlice';
+
+export const productsApiSlice = apiSlice.injectEndpoints({
+    endpoints: (builder) => ({
+        getProducts: builder.query({
+            query: () => ({
+                url: PRODUCT_URL,
+            }),
+            keepUnusedDataFor: 5
+        }),
+        getProductDetails: builder.query({
+            query: (id) => ({
+                url: `${PRODUCT_URL}/${id}`,
+            }),
+            keepUnusedDataFor: 5
+        })
+    })
+})
+
+export const { useGetProductsQuery, useGetProductDetailsQuery } = productsApiSlice;
