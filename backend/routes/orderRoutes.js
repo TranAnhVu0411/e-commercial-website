ddd3fe9715ddf51679aa270a46c8d0c2deaa2c16
TranAnhVu0411 <vuta183674@gmail.com>
2024-07-03 11:35:14 +0700

    Frame for Order (Controller, Route) in Backend

---
+++ backend/routes/orderRoutes.js
@@ -0,0 +1,21 @@
+import express from "express";
+const router = express.Router();
+
+import {
+    addOrderItems,
+    getMyOrder,
+    getOrderById,
+    updateOrderToPaid,
+    updateOrderToDelivered,
+    getOrders
+} from "../controllers/orderController.js";
+
+import { protect, admin } from '../middleware/authMiddleware.js';
+
+router.route('/').post(protect, addOrderItems).get(protect, admin, getOrders);
+router.get('/mine', protect, getMyOrder);
+router.get('/:id', protect, admin, getOrderById);
+router.put('/:id/paid', protect, admin, updateOrderToPaid);
+router.put('/:id/delivered', protect, admin, updateOrderToDelivered);
+
+export default router;
