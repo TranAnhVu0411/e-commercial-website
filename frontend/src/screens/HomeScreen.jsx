eeade8bf00b37d2a21632ee7b14b91995e1283ab
TranAnhVu0411 <vuta183674@gmail.com>
2024-07-05 11:23:24 +0700

    Add Search Function

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,5 +1,5 @@
 import { Row, Col } from 'react-bootstrap';
-import { useParams } from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
 import Product from '../components/Product';
 import { useGetProductsQuery } from '../slices/productsApiSlice';
 import Loader from '../components/Loader';
@@ -7,11 +7,12 @@ import Message from '../components/Message';
 import Paginate from '../components/Paginate';
 
 const HomeScreen = () => {
-    const {pageNumber} = useParams();
-    const { data, isLoading, error } = useGetProductsQuery({pageNumber});
+    const {pageNumber, keyword} = useParams();
+    const { data, isLoading, error } = useGetProductsQuery({keyword, pageNumber});
 
     return (
         <>
+            {keyword && <Link to='/' className='btn btn-light'>Go Back</Link>}
             {
                 isLoading ? (
                     <Loader />
@@ -30,6 +31,7 @@ const HomeScreen = () => {
                                 <Paginate
                                     pages={data.pages}
                                     page={data.page}
+                                    keyword={keyword ? keyword : ''}
                                 />
                     </>
                 )
